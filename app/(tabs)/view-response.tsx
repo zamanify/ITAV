b8ea6ec48808e32244558fddb9d9e2aafa29165d
zamanify <arman@zamany.se>
2025-06-10 12:43:42 +0200

    Updated view-response.tsx

--- app/(tabs)/view-response.tsx
+++ app/(tabs)/view-response.tsx
@@ -354,7 +354,7 @@ export default function ViewResponseScreen() {
         >
           {isSelecting ? (
             <>
-              <ActivityIndicator size="small" color="white" />
+              <ActivityIndicator size="small\" color="white" />
               <Text style={styles.selectButtonText}>Väljer...</Text>
             </>
           ) : (
