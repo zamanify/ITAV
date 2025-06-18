1be994d7bbe139196ca64c1e9c4d656db4e9fe7e
zamanify <arman@zamany.se>
2025-06-18 10:09:00 +0200

    Updated landingPage.tsx

--- app/landingPage.tsx
+++ app/landingPage.tsx
@@ -140,7 +140,7 @@ const styles = StyleSheet.create({
   loginButton: {
     backgroundColor: 'rgba(255, 255, 255, 0.2)',
     paddingVertical: 12,
-    paddingHorizontal: 26,
+    paddingHorizontal: 16,
     borderRadius: 20,
     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 0.5)',
